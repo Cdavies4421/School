238afaaffdefe25c427565a3b836a7e5cd65fd97
unknown <373000996@mvhsrop901-28.vusdnoc.vusd.k12.ca.us>
2015-09-29 13:28:14 -0700

    Initial Commit

--- src/school/Person.java
+++ src/school/Person.java
@@ -1,96 +1,104 @@
 package school;
 import java.util.ArrayList;
-
-public class Person {
-    
-    //These track the instances of Person
-//    public static int numPeople = 10;
-//    private static int currentPeopleIndex = 0;
-//    private static Person people[] = new Person[numPeople];
-    private static ArrayList<Person> people = new ArrayList<Person>();
-    
+import java.util.Calendar;
+public class Person {      
+    protected static ArrayList<Person> people = 
+    new ArrayList<Person>();
     enum Gender {
-        Male, Female // The two genders of the Gender type
+        Male,Female
     }
-    private Gender gender;
     private String name;
     private int weight;
-    
-    public static Person addPerson(String _name, Gender _gender, int _weight){
-        Person temp = new Person(_name, _gender, _weight);
-//        people[currentPeopleIndex++] = temp;
-        people.add(temp);//ArrayList command
+    private Gender gender;
+    private int birthDay;
+    private int birthMonth;
+    private int birthYear;
+
+    public static Person addPerson(String _name,
+    Gender _gender, int _weight)
+    {
+        Person temp = new Person(_name,_gender,_weight);
+        people.add(temp);
         return(temp);
-    }
-    
-    //Default constructor.
-    public Person(){
-        name = "Nohbdy";
-        gender = Gender.Female;
+    }    
+    public static void addPerson(Person _person)
+    {
+        people.add(_person);
+    }    
+    Person()
+    {
+        name = "None";
         weight = 100;
+        gender = Gender.Female;
     }
-    //More specific constructor.
-    public Person(String _Name, Gender _Gender,int _Weight){
-        gender = _Gender;
-        name = _Name;
-        weight = _Weight;
-    }
-    
-    //set and get methods for gender
-    public void setGender(Gender _gender){
-        gender = _gender;
-    }
-    public Gender getGender(){
-        return(gender);
-    }
-    
-    //set and get methods for name
-    public void setName(String _name){
+    Person(String _name,Gender _gender,int _weight)
+    {
         name = _name;
+        weight = _weight;
+        gender = _gender;
+    }  
+    public void setBirthdate
+    (int _day,int _month,int _year)
+    {
+        birthDay = _day;
+        birthMonth = _month;
+        birthYear = _year;
+          
     }
-    public String getName(){
-        return(name);
-    }
-    
-    //set and get methods for weight.
-    public void setWeight(int _weight){
+    public int getAge()
+    {
+        Calendar now = Calendar.getInstance();
+        int day = now.get(Calendar.DAY_OF_MONTH);
+        int month = now.get(Calendar.MONTH) + 1;
+        int year = now.get(Calendar.YEAR);
+        return(0);
+    }    
+    public void setWeight(int _weight)
+    {
         weight = _weight;
     }
-    public int getWeight(){
+    public int getWeight()
+    {
         return(weight);
+    }       
+    public void setName(String _name)
+    {
+        name = _name;
     }
+    public String getName()
+    {
+        return(name);
+    }    
+    public void setGender(Gender _gender)
+    {
+        gender = _gender;
+    }
+    public Gender getGender()
+    {
+        return(gender);
+    }      
     
-    public static void printNames(){
-        System.out.println("====Roster====");
-//        for(int i = 0; i < people.length; i++)
-//        {
-//            if(people[i] != null)
-//            {
-//                System.out.println(people[i].getName());
-//            }
-//        }
-//        for(int i = 0; i < people.size(); i++)
-//        {
-//            System.out.println(people.get(i).getName());
-//        }
-        
-        for(Person temp : people){  //for each loop
+    public static void printNames()
+    {
+        System.out.println("===printNames===");
+        for (Person temp : people)
+        {
             System.out.println(temp.getName());
         }
-    }
-    
-    public static void printNames(Gender gender){
-        System.out.println("====" + gender + " Roster====");
-        for(Person temp : people) //for each loop
-        {  
-            if(temp.getGender() == gender)
-            {
+    }    
+    public static void printNames(Gender _gender)
+    {
+        System.out.println("===printNames===");
+        for (Person temp : people)
+        {
+            if (temp.gender == _gender)
                 System.out.println(temp.getName());
-            }
         }
     }
     
-    public String toString(){
+    
+    public String toString()
+    {
         return(name + " " + gender + " " + weight);
-    }
+    }    
 }
--- src/school/School.java
+++ src/school/School.java
@@ -1,21 +1,42 @@
 package school;
+public class School {
+    public static void main(String[] args) {
+        Student joe =  Student.addStudent("Joe",Person.Gender.Male,120,10);
+        Student jill =  Student.addStudent("Jill",Person.Gender.Female,105,9);
+        Student bobby =  Student.addStudent("Bobby",Person.Gender.Male,168,11);        
+        Student ann =  Student.addStudent("Annie",Person.Gender.Female,98,9);        
+        Student sean =  Student.addStudent("Sean",Person.Gender.Male,138,11);        
+                
+        Teacher price = Teacher.addTeacher("Price", Person.Gender.Male, 206,4.7);
+        Teacher hudson = Teacher.addTeacher("Hudson", Person.Gender.Female, 106,7.2);
+        Teacher anderson = Teacher.addTeacher("Anderson", Person.Gender.Female, 112,3.2);
+        Teacher smith = Teacher.addTeacher("Smith", Person.Gender.Male, 152,8.2);
+        Teacher jones = Teacher.addTeacher("Jones", Person.Gender.Female, 102,1.2);
+        
+        Course geom =  Course.addCourse("Geometry", Course.Type.Math, 1);
+        Course photo =  Course.addCourse("Photography", Course.Type.Elective, 3);       
+        Course biology =  Course.addCourse("Biology", Course.Type.Science, 2);       
+        Course worldHistory =  Course.addCourse("WorldHistory", Course.Type.History, 2);       
 
-public class School 
-{
-
-
-    public static void main(String[] args) 
-    {
-       Person carol = Person.addPerson("Carol", Person.Gender.Female, 115);
-       Person alice = Person.addPerson("Alice", Person.Gender.Female, 125);
-       Person edward = Person.addPerson("Edward", Person.Gender.Male, 180);
-       Person alphonse = Person.addPerson("Alphonse", Person.Gender.Male, 160);
-       carol.setName("Sara"); // These are just variables
-       Person.printNames();
-       System.out.println("\n");
-       Person.printNames(Person.Gender.Female);
-       System.out.println("\n");
-       Person.printNames(Person.Gender.Male);
-       System.out.println("\n" + edward);
+        geom.addStudent(bobby);
+        photo.addStudent(jill);
+        biology.addStudent(ann);
+        worldHistory.addStudent(sean);
+        worldHistory.addStudent(ann);
+        worldHistory.addStudent(joe);
+         
+        geom.addTeacher(price);
+        photo.addTeacher(anderson);
+        biology.addTeacher(smith);
+        worldHistory.addTeacher(hudson);
+        
+        
+        
+        Course.printNames();
+        Person.printNames();
+        Student.printNames();
+        Teacher.printNames();
+        
+        hudson.printStudentsNames();
     }
 }
